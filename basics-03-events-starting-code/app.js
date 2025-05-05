5d91f4d976ea6036f4e8b6b2009a31272d52f699
SebastianJimenez2 <alejandro.jimenez@epn.edu.ec>
2025-05-05 17:29:17 -0500

    feat: eventlistener using Vue

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,8 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
+  },
+  methods: {
+    setName(event) {
+      this.name = event.target.value;
+    },
+    addCounter(num) {
+      this.counter = this.counter + num;
+    },
+    reduceCounter(num) {
+      this.counter = this.counter - num;
+    }
   },
 });
 
-app.mount('#events');
+app.mount("#events");
