feb08c6d62b01e1df5e334ba7b75caf44e3c326a
SebastianJimenez2 <alejandro.jimenez@epn.edu.ec>
2025-05-12 15:22:45 -0500

    feat: slots on Vue.js

--- vue-projects/cmp-adv-01-starting-setup/src/App.vue
+++ vue-projects/cmp-adv-01-starting-setup/src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
   </div>
 </template>
 
 <script>
+import TheHeader from "./components/TheHeader.vue";
+
 export default {
+  components: {
+    TheHeader,
+  },
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
   },
