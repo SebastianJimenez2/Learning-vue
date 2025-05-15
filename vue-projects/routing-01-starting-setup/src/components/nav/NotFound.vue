58d0b20fc40788917c554cebe0781f13e4783840
SebastianJimenez2 <alejandro.jimenez@epn.edu.ec>
2025-05-15 15:09:58 -0500

    feat: routing with Vue

---
+++ vue-projects/routing-01-starting-setup/src/components/nav/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h2> Page not found! </h2>
+        <router-link to="/teams">Teams</router-link>
+    </div>
+</template>
--- vue-projects/routing-01-starting-setup/src/main.js
+++ vue-projects/routing-01-starting-setup/src/main.js
@@ -1,7 +1,48 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 
-const app = createApp(App)
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' }, //redirección de URL raíz no definida
+    // nested route for our.domain/x/x/x/x/x/....
+    {
+      name: 'teams',
+      path: '/teams',
+      components: { default: TeamList, footer: TeamsFooter },
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        },
+      ],
+    },
+    { path: '/users', components: { default: UsersList, footer: UsersFooter } },
+    { path: '/:notFound(.*)', component: NotFound }, //redirección de URLs no definidas
+  ],
+  linkActiveClass: 'active',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return {
+      left: 0,
+      top: 0,
+    };
+  },
+});
 
+const app = createApp(App);
+
+app.use(router);
 app.mount('#app');
