ef038c9a5d0b626610e3161470a7bb6b5c6df7f0
SebastianJimenez2 <alejandro.jimenez@epn.edu.ec>
2025-05-16 18:02:36 -0500

    feat: vuex fundamentals

---
+++ vue-projects/vuex-01-starting-setup/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <base-container title="Vuex">
+  <the-counter></the-counter>
+  <favorite-value></favorite-value>
+  <button @click="addOne">Add 10</button>
+  <change-counter></change-counter>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
+import TheCounter from './components/TheCounter.vue';
+
+
+export default {
+  components: {
+    BaseContainer,
+    ChangeCounter,
+    TheCounter,
+    FavoriteValue
+  },
+  computed: {
+    counter () {
+      return this.$store.state.counter;
+    }
+  },
+  methods: {
+    addOne() {
+      this.$store.dispatch('increase', {value: 10}); // ejecutar la mutación 
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ vue-projects/vuex-01-starting-setup/src/components/FavoriteValue.vue
@@ -0,0 +1,15 @@
+<template>
+    <h3>{{ counter }}</h3>
+    <p>More</p>
+  </template>
+  
+  <script>
+  export default {
+    computed: {
+      counter() {
+        return this.$store.getters.normalizedCounter;
+      },
+    },
+  };
+  </script>
+  
